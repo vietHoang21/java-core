06d7f0dde064ff397b7d7c425ee59eaafeb99a56
vietHoang21 <Hoang21112004@gmail.com>
2024-10-11 16:35:36 +0700

    feat(helloworld): #1 finish task hello world

--- src/main/java/work/vietdefi/helloworld/HelloWorld.java
+++ src/main/java/work/vietdefi/helloworld/HelloWorld.java
@@ -9,5 +9,6 @@ public class HelloWorld {
     public static void main(String[] args){
         DOMConfigurator.configure("config/log/log4j.xml");
         logger.info("Hello world, Java");
+        //fix commit message
     }
 }
