9f7122f9c5b7265ad97922f7666d2c5bc15c9d37
vietHoang21 <Hoang21112004@gmail.com>
2024-10-11 15:05:31 +0700

    feat(hellowordl): #1 finish task hello world

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,6 +10,10 @@ repositories {
 }
 
 dependencies {
+    // https://mvnrepository.com/artifact/org.slf4j/slf4j-api
+    implementation("org.slf4j:slf4j-api:2.0.16")
+    // https://mvnrepository.com/artifact/org.slf4j/slf4j-log4j12
+    implementation("org.slf4j:slf4j-log4j12:2.0.16")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
