09a7a3ffa4efec3aa4cd89e7210c3a7dcd295a96
vietHoang21 <Hoang21112004@gmail.com>
2024-10-23 17:24:26 +0700

    feat(java-dsa): #7 insertion sort

---
+++ src/main/java/work/vietdefi/dsa/sort/InsertionSorter.java
@@ -0,0 +1,18 @@
+package work.vietdefi.dsa.sort;
+
+public class InsertionSorter implements Sorter{
+    @Override
+    public int[] sort(int[] values) {
+        if(values.length <= 1) return null;
+        for (int i = 1; i < values.length; i++) {
+            int temp = values[i];
+            int j = i - 1;
+            while (j >= 0 && values[j] > temp) {
+                values[j + 1] = values[j];
+                j = j - 1;
+            }
+            values[j + 1] = temp;
+        }
+        return values;
+    }
+}
---
+++ src/test/java/work.vietdefi.dsa.sort/InsertionSorterTest.java
@@ -0,0 +1,17 @@
+package work.vietdefi.dsa.sort;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+public class InsertionSorterTest {
+    @Test
+    @DisplayName("insertion sort")
+    public void sort() {
+        Sorter sorter = new InsertionSorter();
+        int[] test = {28,1,8,18,24,72,80,73,14,55};
+        int[] actualArray = {1,8,14,18,24,28,55,72,73,80};
+        assertArrayEquals(sorter.sort(test), actualArray);
+    }
+}
