d98b346a50c15ee4c940475ba6e22d8a601d46c7
vietHoang21 <Hoang21112004@gmail.com>
2024-10-24 15:33:38 +0700

    feat(java-dsa): #7 bubble sort

--- src/main/java/work/vietdefi/dsa/sort/BubbleSorter.java
+++ src/main/java/work/vietdefi/dsa/sort/BubbleSorter.java
@@ -23,7 +23,7 @@ public class BubbleSorter implements Sorter{
 
     @Override
     public int[] sort(int[] values) {
-        if(values.length <= 1) return null;
+        if(values.length <= 1) return values;
         int n = values.length;
         for (int i = 0; i < n - 1; i++) {
             for (int j = 0; j < n - i - 1; j++) {
